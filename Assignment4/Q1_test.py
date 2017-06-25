a7be1dc42ef86ad6b06983d5355e1098362f1c51
pistacja96 <kalisz.ola@gmail.com>
2017-06-25 23:48:32 +0100

    Adding comments and changing tests slightly.

--- Assignment4/Q1_test.py
+++ Assignment4/Q1_test.py
@@ -5,46 +5,49 @@ import unittest
 import Q1
 
 
-class Q1Test(unittest.TestCase):
-    def setUp(self):
-        # Creating some map grid testing examples
-        self._empty_map = [[]]
-        self._no_islands_map = [[False for i in range(5)] for j in range(7)]
-        self._all_land_map = [[True for i in range(10)] for j in range(4)]
-        self._given_map = [[False, True, False, True],
-                           [True, True, False, False],
-                           [False, False, True, False],
-                           [False, False, True, False]]
-        self._test_map_1 = [[False, True, False, False, False],
-                            [False, True, True, True, True],
-                            [True, True, False, False, False],
-                            [False, False, True, True, False]]
-        self._test_map_2 = [[True, True, True, True, True],
-                           [True, False, False, False, True],
-                           [True, False, True, False, True],
-                           [True, False, False, False, True],
-                           [True, True, True, True, True]]
+def _map_grid(string):
+    """Creates a 2D array of characters from a string. The string represents the grid by having the T/F
+       characters corresponding to boolean True/False in each row separated by a '|'.
 
+        Args:
+            string: a string, representation of the grid as a single string T for True, F for False, no other
+            characters allowed (If any other characters are used they will be converted to False).
+
+        Returns:
+            a 2-D array of booleans corresponding to the map grid given.
+    """
+    map_grid = []
+    for row in string.split('|'):
+        map_grid.append([True if ch == 'T' else False for ch in row])
+        # Make sure each row has the same length.
+        if len(map_grid[0]) != len(map_grid[-1]):
+            raise ValueError("not all rows have the same length")
+    return map_grid
+
+
+class Q1Test(unittest.TestCase):
     def testEmptyCase(self):
         """Tests the case when map argument is empty."""
-        self.assertEquals(0, Q1.counting_islands(self._empty_map))
+        self.assertEquals(0, Q1.counting_islands(_map_grid("")))
 
     def testNoIslandsCase(self):
         """Tests the case when there are no islands in a map grid"""
-        self.assertEquals(0, Q1.counting_islands(self._no_islands_map))
+        no_islands_map = [[False for i in range(5)] for j in range(7)]
+        self.assertEquals(0, Q1.counting_islands(no_islands_map))
 
     def testAllLandCase(self):
         """Tests the case when all map tiles are land"""
-        self.assertEquals(1, Q1.counting_islands(self._all_land_map))
+        all_land_map = [[True for i in range(10)] for j in range(4)]
+        self.assertEquals(1, Q1.counting_islands(all_land_map))
 
     def testKnownCases(self):
         """Tests some known cases."""
         # Test the given example
-        self.assertEquals(3, Q1.counting_islands(self._given_map))
+        self.assertEquals(3, Q1.counting_islands(_map_grid("FTFT|TTFF|FFTF|FFTF")))
         # Test other map example
-        self.assertEquals(2, Q1.counting_islands(self._test_map_1))
+        self.assertEquals(2, Q1.counting_islands(_map_grid("FTFFF|FTTTT|TTFFF|FFTTF")))
         # Test other map example - ring island
-        self.assertEquals(2, Q1.counting_islands(self._test_map_2))
+        self.assertEquals(2, Q1.counting_islands(_map_grid("TTTTT|TFFFT|TFTFT|TFFFT|TTTTT")))
 
 
 if __name__ == '__main__':
